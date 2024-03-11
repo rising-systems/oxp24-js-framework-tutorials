mairasalazar/tutorials

[Solution] 1.15 Add commands in command palette

awesome_clicker/static/src/clicker_provider.js
@@ -1,0 +1,29 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+
+const commandProviderRegistry = registry.category("command_provider");
+
+commandProviderRegistry.add("clicker", {
+    provide: (env, options) => {
+        return [
+            {
+                name: "Buy 1 click bot",
+                action() {
+                    env.services["awesome_clicker.clicker_service"].buyBot("clickbot");
+                },
+            },
+            {
+                name: "Open Clicker Game",
+                action() {
+                    env.services.action.doAction({
+                        type: "ir.actions.client",
+                        tag: "awesome_clicker.client_action",
+                        target: "new",
+                        name: "Clicker Game",
+                    });
+                },
+            }
+        ]
+    },
+});

rising-systems/oxp24-js-framework-tutorials

awesome_clicker/static/src/clicker_provider.js
@@ -1,0 +1,29 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+
+const commandProviderRegistry = registry.category("command_provider");
+
+commandProviderRegistry.add("clicker", {
+    provide: (env, options) => {
+        return [
+            {
+                name: "Buy 1 click bot",
+                action() {
+                    env.services["awesome_clicker.clicker"].buyBot("clickbot");
+                },
+            },
+            {
+                name: "Open Clicker Game",
+                action() {
+                    env.services.action.doAction({
+                        type: "ir.actions.client",
+                        tag: "awesome_clicker.client_action",
+                        target: "new",
+                        name: "Clicker Game",
+                    });
+                },
+            }
+        ]
+    },
+});