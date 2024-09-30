ec801f9b244e8666d38734eb90cb313040e3b503
lflfm <26166377+lflfm@users.noreply.github.com>
2024-09-30 11:20:59 +0200

    ex. 5 - added props validation
    https://github.com/odoo/tutorials/blob/master-js-smartclass-2024/1_learn_owl.md#5-props-validation

--- awesome_owl/static/src/card.js
+++ awesome_owl/static/src/card.js
@@ -3,5 +3,11 @@ import { Component, useState } from "@odoo/owl";
 export class Card extends Component {
 	static template = "awesome_owl.Card";
 
+	static props = {
+		// https://github.com/odoo/owl/blob/master/doc/reference/props.md#props-validation
+		title: { type: String, optional: true, default: "Title" },
+		content: { type: String, optional: true, default: "Content" },
+	};
+
 	setup() {}
 }
