30a2eac7975d535264dc738151e01094f94a6a54
lflfm <26166377+lflfm@users.noreply.github.com>
2024-09-30 11:08:30 +0200

    ex.4 using markup function to display html content https://github.com/odoo/tutorials/blob/master-js-smartclass-2024/1_learn_owl.md#4-using-markup-to-display-html

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,12 @@
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter";
 import { Card } from "./card";
 
 export class Playground extends Component {
 	static template = "awesome_owl.playground";
 	static components = { Counter, Card };
+
+	setup() {
+		this.htmlcontent = markup`<a href="http://rs.ag">rising sysstems</a>`;
+	}
 }
