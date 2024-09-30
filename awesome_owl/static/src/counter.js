b4b1bc9b2e98769a91e748eaabe5813e87ff171a
lflfm <26166377+lflfm@users.noreply.github.com>
2024-09-30 10:05:42 +0200

    ex.1 - Counter component

---
+++ awesome_owl/static/src/counter.js
@@ -0,0 +1,13 @@
+import { Component, useState } from "@odoo/owl";
+
+export class Counter extends Component {
+	static template = "my_module.Counter";
+
+	setup() {
+		this.state = useState({ value: 0 });
+	}
+
+	increment() {
+		this.state.value++;
+	}
+}
