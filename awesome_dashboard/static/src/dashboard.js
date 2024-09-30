862f1f1d173c0ab8412eb2c52e9e5a6991b14a27
lflfm <26166377+lflfm@users.noreply.github.com>
2024-09-30 16:28:33 +0200

    4. Call the server, add some statistics
    https://github.com/odoo/tutorials/blob/master-js-smartclass-2024/2_make_a_dashboard.md#4-call-the-server-add-some-statistics

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,9 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
 import { DashboardItem } from "./dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
 	static template = "awesome_dashboard.AwesomeDashboard";
@@ -13,6 +14,27 @@ class AwesomeDashboard extends Component {
 			controlPanel: {}, //disabling the control panel
 		};
 		this.action = useService("action");
+        this.state = useState({
+            average_quantity: 0,
+            average_time: 0,
+            nb_cancelled_orders: 0,
+            nb_new_orders: 0,
+            orders_by_size: {
+                m: 0,
+                s: 0,
+                xl: 0,
+            },
+            total_amount: 0,
+        });
+		onWillStart(async () => {
+			const result = await rpc("/awesome_dashboard/statistics", {});
+            this.state.average_quantity = result.average_quantity;
+            this.state.average_time = result.average_time;
+            this.state.nb_cancelled_orders = result.nb_cancelled_orders;
+            this.state.nb_new_orders = result.nb_new_orders;
+            this.state.orders_by_size = result.orders_by_size;
+            this.state.total_amount = result.total_amount;
+		});
 	}
 
 	openSettings() {
