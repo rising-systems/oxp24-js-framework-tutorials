93ac09ca8c6d4799d4782150c97552f92e6ca89e
lflfm <26166377+lflfm@users.noreply.github.com>
2024-09-30 15:32:07 +0200

    dashboard ex.1 - wrapping in Odoo client Layout
    https://github.com/odoo/tutorials/blob/master-js-smartclass-2024/2_make_a_dashboard.md#1-a-common-layout

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,18 @@
 import { Component } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+	static template = "awesome_dashboard.AwesomeDashboard";
+	static components = { Layout };
+
+	setup() {
+		this.display = {
+			controlPanel: {}, //disabling the control panel
+		};
+	}
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+	.category("actions")
+	.add("awesome_dashboard.dashboard", AwesomeDashboard);
