dde39525d99deb78fe1a6739c572795a8519c135
lflfm <26166377+lflfm@users.noreply.github.com>
2024-10-01 11:18:03 +0200

    3. Create a client action
    https://github.com/odoo/tutorials/blob/master-js-smartclass-2024/3_build_a_clicker_game.md#3-create-a-client-action

--- awesome_clicker/static/src/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item.js
@@ -1,11 +1,13 @@
 import { registry } from "@web/core/registry";
 import { Component, useState, useExternalListener } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 
 export class ClickerSystray extends Component {
 	static template = "awesome_clicker.ClickerSystray";
 	static props = {};
 	setup() {
 		this.state = useState({ counter: 0 });
+		this.action = useService("action");
 		//useExternalListener already has the teardown logic built-in
 		// the last "true" argument is to use the capture, related to the event propagation
 		useExternalListener(document.body, "click", this.increment, true);
@@ -14,6 +16,15 @@ export class ClickerSystray extends Component {
 	increment() {
 		// we increment here by 9 so that the user get 10 (1 for the general body click)
 		this.state.counter += 9;
+	}
+
+	openClientAction() {
+		this.action.doAction({
+			type: "ir.actions.client",
+			tag: "awesome_clicker.client_action",
+			target: "new",
+			name: "Clicker Game",
+		});
 	}
 }
 export const systrayItem = {
